c486bad1241836868d7ce6478b46dce165ebe501
yashap <ypodeswa@gmail.com>
2014-05-06 09:24:29 -0700

    Added basic front end for wiki functionality, but you can't yet edit posts

--- handlers/editPage.py
+++ handlers/editPage.py
@@ -3,12 +3,12 @@
 import handler
 from entities import Page
 
-class showPage(handler.Handler):
+class EditPage(handler.Handler):
 	def get(self, page_id):
 		# in main.py, page_id is automatically passed to the handler
 		q = Page.Page.by_page_id(page_id)
 		if q:
-			self.render("page.html", title=q.title, content=q.content, created=q.created, last_modified=q.last_modified)
+			self.render("edit_page.html", title=q.title, content=q.content, created=q.created, last_modified=q.last_modified)
 
 		else:
-			self.redirect("/_edit%s" % page_id)
+			self.render("edit_page.html", title="filler", content="filler", created="filler", last_modified="filler")
