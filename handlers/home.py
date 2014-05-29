542a272dbf3687e5b48144b5730bbb8b14ab1f69
yashap <ypodeswa@gmail.com>
2014-05-29 09:22:41 -0700

    Further character cutoff bugfix

--- handlers/home.py
+++ handlers/home.py
@@ -8,12 +8,13 @@ class Home(handler.Handler):
 	def get(self):
 		top_pages = Page.Page.top_10()
 		for i in range(len(top_pages)):
-			content = top_pages[i].content
-			cutoff = 300
-			line_count = content.count("\n")
+			max_chars = 300
 			max_lines = 3
-			if len(content) > cutoff or line_count > max_lines:
+			content = top_pages[i].content
+			line_count = content.count("\n")
+			if len(content) > max_chars or line_count > max_lines:
 				pos = utils.findnth(content, "\n", max_lines) - 1
-				cutoff = min(pos, cutoff)
+				cutoff = min(pos, max_chars) if pos > 5 else max_chars
+				print("pos: %s; max_chars: %s; cutoff: %s" % (pos, max_chars, cutoff))
 				top_pages[i].content = content[:cutoff] + " ..."
 		self.render("home.html", top_pages=top_pages)
